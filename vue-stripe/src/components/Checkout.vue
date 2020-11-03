9da73b26468c092e503a53492855e174c4909cf3
ZachyDev <mosetizachary001@gmail.com>
2020-11-04 00:32:43 +0300

    getting started with Stripe elements with Vue

---
+++ vue-stripe/src/components/Checkout.vue
@@ -0,0 +1,33 @@
+<template>
+<stripe-checkout ref="checkoutRef" :pk="publishableKey" :items="items" :successUrl="successUrl" :cancelUrl="cancelUrl">
+    <template slot="checkout-button">
+        <button @click="checkout">Shut up and take my money!</button>
+    </template>
+</stripe-checkout>
+</template>
+
+<script>
+import {
+    StripeCheckout
+} from 'vue-stripe-checkout';
+export default {
+    components: {
+        StripeCheckout
+    },
+    data: () => ({
+        loading: false,
+        publishableKey: "pk_test_51HjMvBAKC7vfWzcLuhviBABBa61jqs3XzVqRM9Uc6wWZ20sL21gXkpC77Nnum3J7muJ1CVWW9S9SenE6APXwfPfX00ricpXErF",
+        items: [{
+            sku: 'sku_FdQKocNoVzznpJ',
+            quantity: 1
+        }],
+        successUrl: 'https://codewithzachy.netlify.com',
+        cancelUrl: 'https://rekodi.netlify.app',
+    }),
+    methods: {
+        checkout() {
+            this.$refs.checkoutRef.redirectToCheckout();
+        }
+    }
+}
+</script>
